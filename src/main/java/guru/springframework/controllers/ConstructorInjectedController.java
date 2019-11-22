ca5536f2e8d37b40e570b1e6e2b50b017bf8e403
RetlavSource <valterinfomundo@gmail.com>
2019-11-22 12:21:29 +0000

    lesson 38

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -2,16 +2,21 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired // this could exist or not, because it has already a constructor for the attributes
     public ConstructorInjectedController(GreetingService greetingService){
         this.greetingService = greetingService;
     }
 
-    String sayHello(){
+    public String sayHello(){
         return greetingService.sayGreeting();
     }
 
