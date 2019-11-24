67dbaed25420adc1e5152cf3f2162716fce6e6d8
RetlavSource <valterinfomundo@gmail.com>
2019-11-24 07:36:49 +0000

    lesson 39

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -1,8 +1,7 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 
@@ -11,8 +10,7 @@ public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
-    @Autowired // this could exist or not, because it has already a constructor for the attributes
-    public ConstructorInjectedController(GreetingService greetingService){
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService){
         this.greetingService = greetingService;
     }
 
--- src/main/java/guru/springframework/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/controllers/SetterInjectedController.java
@@ -3,6 +3,7 @@ package guru.springframework.controllers;
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 
@@ -16,7 +17,8 @@ public class SetterInjectedController {
     }
 
     @Autowired
-    public void setGreetingService(GreetingService greetingService){
+    //@Qualifier("setterGreetingService") // can be used also in the method like the constructor service
+    public void setGreetingService(@Qualifier("setterGreetingService") GreetingService greetingService){
         this.greetingService = greetingService;
     }
 
